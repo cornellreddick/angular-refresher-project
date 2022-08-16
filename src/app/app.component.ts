c2fc5a1a1c6886b01cb1afafc7161afb051821ab
Cornell Reddick <cornellreddick@outlook.com>
2022-08-16 10:59:26 -0400

    Adding name properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-refresher-project';
+  title = 'Angular Refresher';
+  
+  firstName = 'Cornell';
+  lastName = 'Reddick';
+
 }
